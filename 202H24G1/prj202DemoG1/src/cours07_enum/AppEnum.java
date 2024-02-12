dc43393e29095a10387282c5d88cad6a10223297
Robert Aubé <robert.aube@cegeplimoilou.ca>
2024-02-12 10:41:56 -0500

    G1 - Prep Cours 7

---
+++ 202H24G1/prj202DemoG1/src/cours07_enum/AppEnum.java
@@ -0,0 +1,18 @@
+package cours07_enum;
+
+public class AppEnum {
+    public AppEnum() {
+        demoCours202();
+    }
+
+    private void demoCours202() {
+        demoEnum();
+    }
+
+    private void demoEnum() {
+    }
+
+    public static void main(String[] args) {
+        new AppEnum();
+    }
+}
